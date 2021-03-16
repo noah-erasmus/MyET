84209f0bafbc6abb907e8f37d993e7fa2c680cad
noah-erasmus <55457778+noah-erasmus@users.noreply.github.com>
2021-03-16 19:11:53 +0200

    Begin Food State

--- MyET/MyET/Objects/FoodState.cs
+++ MyET/MyET/Objects/FoodState.cs
@@ -5,12 +5,50 @@ using System.Text;
 namespace MyET.Objects
 {
 
-    public enum FoodStates
+    public enum FoodState
     {
-
+        wellfed,
+        content,
+        hungry,
+        starving
     }
 
-    class FoodState
+    class FoodStates
     {
+
+        public static string GetFoodString(FoodState foodState)
+        {
+            switch(foodState)
+            {
+                case FoodState.wellfed:
+                    return "well fed";
+                case FoodState.content:
+                    return "content";
+                case FoodState.hungry:
+                    return "hungry";
+                case FoodState.starving:
+                    return "starving";
+                default:
+                    return "unborn";
+            }
+        }
+
+        public static FoodState GetFoodState(string foodString)
+        {
+            switch(foodString)
+            {
+                case "well fed":
+                    return FoodState.wellfed;
+                case "content":
+                    return FoodState.content;
+                case "hungry":
+                    return FoodState.hungry;
+                case "starving":
+                    return FoodState.content;
+                default:
+                    return FoodState.content;
+            }
+        }
     }
+
 }
---
+++ MyET/MyET/Objects/Levels.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyET.Objects
+{
+    class Levels
+    {
+        public static int GetLevelFromXp(int xp)
+        {
+            if(xp == 0)
+            {
+                return 0;
+            }else if(xp < 2000)
+            {
+                return 1;
+            }else if(xp >= 2000)
+            {
+                return 2;
+            }else
+            {
+                return 0;
+            }
+            
+        }
+    }
+}
